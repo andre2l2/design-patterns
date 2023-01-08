f6d5e95c77bd10578dd9bfc09906fcb4ca3bedef
Andre Ferreira <delcferreira@gmail.com>
2023-01-08 19:33:23 -0300

    fix: remove main classes

--- src/Main.java
+++ src/Main.java
@@ -4,19 +4,6 @@ import strategy.ducks.RubberDuck;
 
 public class Main {
     public static void main(String[] args) {
-        MallardDuck mallardDuck = new MallardDuck();
-        mallardDuck.display();
-        mallardDuck.swim();
-        mallardDuck.performFly();
 
-        RedHeadDuck redHeadDuck = new RedHeadDuck();
-        redHeadDuck.display();
-        redHeadDuck.swim();
-        redHeadDuck.performFly();
-
-        RubberDuck rubberDuck = new RubberDuck();
-        rubberDuck.display();
-        rubberDuck.swim();
-        rubberDuck.performFly();
     }
 }
